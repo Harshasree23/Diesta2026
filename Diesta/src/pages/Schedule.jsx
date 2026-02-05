8b74a96332ff4f662f4fb9ee4a739c295805ace4
sohansai <76840110+sohansai@users.noreply.github.com>
2026-02-05 10:56:28 +0530

    testing-commit-schedule-v0

--- Diesta/src/pages/Schedule.jsx
+++ Diesta/src/pages/Schedule.jsx
@@ -3,6 +3,7 @@ import '../styles/Schedule.css';
 
 const API_URL = "https://script.google.com/macros/s/AKfycbxHGE7PDuL0JCS2Ki0_-A_6h8RK9H6OTTbnDSh9Q6vs1xq-4Dwhz6QnCoGP1CbmFfg1/exec";
 
+
 const Schedule = () => {
   const [selectedDate, setSelectedDate] = useState('Feb 6');
   const [searchQuery, setSearchQuery] = useState('');
